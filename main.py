930e68af9d0773e7f33ffc5831bdaf200eca5bef
ahmedtom <101574426+ahmedtom@users.noreply.github.com>
2022-06-21 20:45:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/YukkiChatBot >.
+# Copyright (C) 2021-2022 by Tom@Github, < https://github.com/ahmedtom >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiChatBot > project,
+# This file is part of < https://github.com/ahmedtom/tom-chat > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiChatBot/blob/master/LICENSE >
+# Please see < https://github.com/ahmedtom/ahmed-chat >
 #
 # All rights reserved.
 #
